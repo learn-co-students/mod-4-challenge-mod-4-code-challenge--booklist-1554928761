c7e6831eb1085c0a0b4b7b98290bc70b8f95273e
Nat-Reid <nathanielreid@gmail.com>
2019-04-10 15:17:27 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -5,11 +5,43 @@ import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      books: []
+    }
+
+    fetch("http://localhost:3005/books")
+    .then(response => {
+      return response.json()
+    })
+    .then(books => {
+      this.setState({books})
+    });
+  }
+
+  newBook = (book) => {
+    this.setState(prevState => {
+      let updatedBooks = [...prevState.books]
+      updatedBooks.unshift(book)
+      return {books: updatedBooks}
+    })
+  }
+
+  moveBook = (book) => {
+    book.onShelf = !book.onShelf
+    this.forceUpdate()
+  }
+
   render() {
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList books={this.state.books}
+                  moveBook={this.moveBook}
+                  newBook={this.newBook}/>
+        <Bookshelf books={this.state.books.filter(book => book.onShelf)}
+                   moveBook={this.moveBook}/>
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,42 @@
 import React from "react";
 
 class Form extends React.Component {
+
+  state = {
+    title: '',
+    author: '',
+    img: ''
+  }
+
+  renderInputs(){
+    return Object.keys(this.state).map((field, index) => {
+      return <label key={index}>
+        {field.toUpperCase()}
+        <input type="text"
+          name={field}
+          value={this.state[field]}
+          onChange={this.handleChange}/>
+      </label>
+    })
+  }
+
+  handleChange = (ev) => {
+    this.setState({[ev.target.name]: ev.target.value})
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.props.newBook(this.state)
+  }
+
   render() {
-    return <h1>{/*create form*/}</h1>;
+    return <div>
+      <h2>Add a New Book</h2>
+      <form onSubmit={this.handleSubmit}>
+        {this.renderInputs()}
+        <input type="submit" name="Submit"/>
+      </form>
+    </div>;
   }
 }
 
