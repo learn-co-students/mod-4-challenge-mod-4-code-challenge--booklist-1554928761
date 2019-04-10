9858c54baf2be7c9a5ff322b0880b87f6f5eb27c
Katie Carrel <katiecarrel@comcast.net>
2019-04-10 15:13:59 -0700

    i tried my darndest

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,28 @@
 import React from "react";
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: '',
+      title: '',
+      author: '',
+      img: ''
+    }
+  }
+
+  handleChange(ev) {
+    this.setState({[ev.target.name]: ev.target.value})
+  }
   render() {
-    return <h1>{/*create form*/}</h1>;
+    return <h1>{
+        <form onSubmit={(ev) => this.props.handleSubmit(ev, this.state.title,this.state.author,this.state.img,)}>
+          <input type='text' placeholder='title' name='title' value={this.state.title} onChange={(ev) => this.handleChange(ev)}></input>
+          <input type='text' placeholder='author' name='author' value={this.state.author} onChange={(ev) => this.handleChange(ev)}></input>
+          <input type='text' placeholder='img' name='img' value={this.state.img} onChange={(ev) => this.handleChange(ev)}></input>
+          <input type='submit'></input>
+        </form>
+      }</h1>;
   }
 }
 
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,12 +3,59 @@ import Book from "../components/Book";
 import Form from "../components/Form";
 
 class BookList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formBooks: []
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+  }
+
+  //cannot get the new form stuff to show on page without re-rendering the page in constructor
+  handleSubmit(ev, newTitle, newAuthor, newImg) {
+    ev.preventDefault();
+    let newBooks = {
+      id: this.props.books.length + 1,
+      title: newTitle,
+      author: newAuthor,
+      img: newImg
+    }
+    this.state.formBooks.push(newBooks)
+    this.setState({
+      formBooks: this.state.formBooks
+    })
+  }
+
   render() {
     return (
       <div className="book-list">
         <h1>Book List</h1>
-        <Form />
-        <ul>{/*render list of books here*/}</ul>
+        <Form handleSubmit={this.handleSubmit}/>
+        <ul> {
+            this.state.formBooks.map(book => {
+              return <Book
+
+                onClick={(ev) => this.props.handleClick(ev)}
+                action='add'
+                handleClick={this.props.handleClick}
+                 book={book}
+
+                 key={this.props.books.length + 1}/>
+            })
+          }
+          {
+          this.props.books.map(book => {
+            return <Book
+
+              onClick={(ev) => this.props.handleClick(ev)}
+              action='add'
+              handleClick={this.props.handleClick}
+               book={book}
+               key={book.id}/>
+          })
+        }
+          </ul>
       </div>
     );
   }
