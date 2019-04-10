70d49d3887f8e18152e194e4cad8569bf0733076
ScottDenton <scottyd_14@hotmail.com>
2019-04-10 14:23:28 -0700

    All the important stuff done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      books: [],
+      bookshelf: [],
+      title: '',
+      author: '',
+      image: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3005/books')
+    .then(resp => resp.json())
+    .then(books => this.setState({books}))
+  }
+
+  handleClick = (book) => {
+    this.setState({
+      bookshelf: [...this.state.bookshelf, book]
+    })
+  }
+
+  removeBook =(book) => {
+    const updatedBooks = this.state.bookshelf.filter(thisBook => {return book !== thisBook})
+    this.setState({
+      bookshelf: updatedBooks
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) =>{
+    e.preventDefault();
+    this.setState({
+      books:[...this.state.books, {title: this.state.title, author: this.state.author, img: this.state.image}]
+    })
+  }
+
   render() {
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList
+          books={this.state.books}
+          handleClick={this.handleClick}
+          title={this.state.title}
+          author={this.state.author}
+          image={this.state.image}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          />
+        <Bookshelf books={this.state.bookshelf}
+          handleClick={this.removeBook}/>
       </div>
     );
   }
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -7,8 +7,17 @@ class BookList extends Component {
     return (
       <div className="book-list">
         <h1>Book List</h1>
-        <Form />
-        <ul>{/*render list of books here*/}</ul>
+        <Form
+          title={this.props.title}
+          author={this.props.author}
+          image={this.props.image}
+          handleChange={this.props.handleChange}
+          handleSubmit={this.props.handleSubmit}/>
+        <ul>{this.props.books.map(book => (
+            <Book book={book}
+              key={book.title}
+              handleClick={this.props.handleClick}/>
+          ))}</ul>
       </div>
     );
   }
