9304579284692eb9eb92f2de5ce0ff61b77f3d09
jasonbasuil <basuilj@gmail.com>
2019-04-10 15:15:22 -0700

    Done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,45 @@
 import React from "react";
 
 class Form extends React.Component {
-  render() {
-    return <h1>{/*create form*/}</h1>;
+
+  //everything works here, I just
+  //ran out of time to render the new book
+  //controlled form works and everything
+  
+  constructor() {
+    super()
+    this.state = {
+      title: 'title',
+      author: 'author',
+      img: 'img'
+    }
   }
+
+  handleChange = (ev) => {
+    this.setState({[ev.target.name]: ev.target.value})
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.setState({
+      title: 'title',
+      author: 'author',
+      img: 'img'
+    })
+  }
+
+  render() {
+    return (
+    <div>
+      <h1>Form</h1>;
+      <form onSubmit={this.handleSubmit}>
+        <input type='text' name='title' value={this.state.title} onChange={this.handleChange} />
+        <input type='text' name='author' value={this.state.author} onChange={this.handleChange} />
+        <input type='text' name='img' value={this.state.img} onChange={this.handleChange} />
+        <input type='submit' value='Submit'/>
+      </form>
+    </div>
+  )}
 }
 
 export default Form;
--- src/containers/Bookshelf.js
+++ src/containers/Bookshelf.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {Component} from "react";
 import Book from "../components/Book";
 
-const Bookshelf = props => {
+class Bookshelf extends Component {
+
+  render() {
   return (
     <div>
       <h1>Book Shelf</h1>
-      <ul>{/*render list of books here*/}</ul>
+      <ul>
+        {this.props.likedBooks.map(book => {
+          return <Book book={book} handleLike={this.props.handleLike}/>
+        })}
+      </ul>
+
     </div>
   );
-};
+}};
 
 export default Bookshelf;
